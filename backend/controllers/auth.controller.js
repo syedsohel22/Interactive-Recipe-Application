3645d4051c2cb54d1dfa45dc291806741235911e
sohel <mr.sohelsyed@gmail.com>
2023-10-20 17:47:46 +0530

    global error handler

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -8,7 +8,7 @@ const register = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
 
--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,16 @@ app.use(cors());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 /*******************************************************************************************************************/
 app.listen(port, async () => {
   try {
